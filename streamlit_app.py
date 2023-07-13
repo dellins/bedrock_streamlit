4618727bf06900fd041acdaf6a4f59900a2c8d4e
dellins <oyeyemidellins@gmail.com>
2023-07-13 18:08:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 
 import streamlit as st
 
+def consume_url(message):
+    url = "https://y9s346bpuj.execute-api.us-east-1.amazonaws.com/default/bedrock_opensearch_demo"
+    body = {
+        "message": message
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+    
+    if response.status_code == 200:
+        data = response.json()
+
+        return data
+        # Process the data as needed
+        print(data)
+    else:
+        print("Failed to fetch data. Status code:", response.status_code)
+
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -12,7 +29,7 @@ if prompt := st.chat_input():
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = "output" #openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
+    response = consume_url(prompt) #openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.body.message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
