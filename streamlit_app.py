5206d9ae0adfc8a3a644ecff7baa170058895163
dellins <oyeyemidellins@gmail.com>
2023-07-13 18:01:22 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = "output" #openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
